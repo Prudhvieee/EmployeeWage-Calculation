ef63e89bc42ff4284a7d9155e59235e378aed46f
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-09 18:44:45 +0530

    [Prudhvi] add . adding partTime employee

--- src/UC-1_CheckEmployeeIsPresentOrAbsent.js
+++
@@ -1,8 +0,0 @@
-// UC1 check if the employee is present or not
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
-} else {
-    console.log("employee is present");
-}
