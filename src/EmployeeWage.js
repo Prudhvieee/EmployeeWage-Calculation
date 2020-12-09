f3aab103d4b940c6df9cc28be58c5637473d8401
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-09 19:22:35 +0530

    [Prudhvi] add . Adding maximum working hours

--- src/EmployeeWage.js
+++ src/EmployeeWage.js
@@ -1,19 +1,11 @@
-// UC1 check if the employee is present or not
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
-} else {
-    console.log("employee is present");
-}
-
-// UC2 Added full time and part time employee wage
+// UC5 Added number of working hours equal to 160 
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
-
+const MAX_WORK_HRS = 160;
+const Number_Of_Working_Days = 20;
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -26,21 +18,14 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empWage = getWorkingHours(empCheck) * WAGE_PER_HR;
-console.log("Emp wage:" + empWage);
-
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
-    default:
-        empHrs = 0;
-        break;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs < MAX_WORK_HRS && totalWorkingDays < Number_Of_Working_Days) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
 }
-let empWage = empHrs * WAGE_PER_HR;
-console.log("Emp wage:" + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HR;
+console.log("Total days: " + totalWorkingDays +
+    " Total hrs: " + totalEmpHrs + " emp wage: " + empWage);
